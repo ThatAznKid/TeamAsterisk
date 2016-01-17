9fd1718363a431d262c0c9d0177b164192d92a29
Rickx777 <Cwong7@stuy.edu>
2016-01-17 13:31:56 -0500

    movement added to map.java

--- Map.java
+++ Map.java
@@ -1,30 +1,37 @@
-public class Map { 
+import java.io.*;
+import java.util.*;
+
+public class Map {
+    private static InputStreamReader isr;
+    private static BufferedReader in;
     
     private static final Object[][] _map = {{"o","o","o"," "," "," "," "," "," ","o","o","o"},
- 			       {"o","T","o","+","+","=","=","+","+","o","A","o"},
- 			       {"o","o","o"," "," "," "," "," "," ","o","o","o"},
- 			       {" ","+"," "," "," ","B","=","+"," "," ","+"," "},
-			       {" ","|"," "," "," "," "," ","|"," "," ","|"," "},
- 			       {" ","+","=","=","+"," "," ","|"," "," ","|"," "},
- 			       {" ","|"," "," ","+"," ","+","+"," "," ","|"," "},
- 			       {" ","+"," "," ","+","=","+"," "," "," ","+"," "},
- 			       {" ","+"," "," "," "," "," "," "," "," ","+"," "},
- 			       {"o","o","o"," "," "," "," "," "," ","o","o","o"},
-   			       {"o","R","o","+","+","=","=","+","+","o","C","o"},
- 			       {"o","o","o"," "," "," "," "," "," ","o","o","o"}};
+					    {"o","T","o","+","+","=","=","+","+","o","A","o"},
+					    {"o","o","o"," "," "," "," "," "," ","o","o","o"},
+					    {" "," "," "," "," ","B","=","+"," "," ","+"," "},
+					    {" "," "," "," "," "," "," ","|"," "," ","|"," "},
+					    {" ","+","=","=","+"," "," ","|"," "," ","|"," "},
+					    {" ","|"," "," ","+"," ","+","+"," "," ","|"," "},
+					    {" ","+"," "," ","+","=","+"," "," "," ","+"," "},
+					    {" ","+"," "," "," "," "," "," "," "," ","+"," "},
+					    {"o","o","o"," "," "," "," "," "," ","o","o","o"},
+					    {"o","R","o","+","+","=","=","+","+","o","C","o"},
+					    {"o","o","o"," "," "," "," "," "," ","o","o","o"}};
     private int xcord;
     private int ycord;
     
     public Map(){
-	xcord = 1;
-	ycord = 1;
+	isr = new InputStreamReader( System.in );
+    	in = new BufferedReader( isr );
+	xcord = 0;
+	ycord = 0;
     }
 
     public String toString() {
 	String s = "";
 	for (int x = 0; x < _map.length; x++) { 
 	    for (int y = 0; y < _map[x].length; y++) {
-		if (x == xcord && y == ycord){
+		if (x == ycord && y == xcord){
 		    s += " x ";
 		}
 		else s += " " + _map[x][y] + " "; 
@@ -48,13 +55,46 @@ public class Map {
     public void sety(int newy) {
 	ycord = newy;
     }
+
+    //movement
+    public void move(String dir){
+	if (dir.equals("w") && (ycord - 1) >= 0){
+	    if (_map[ycord - 1][xcord].equals(" ")){
+		    return;}
+	    else sety(ycord - 1);
+	}
+	if (dir.equals("s") && (ycord + 1) < 12){ 
+	    if (_map[ycord + 1][xcord].equals(" ")){
+		    return;}
+	    else sety(ycord + 1);
+	}
+
+	if (dir.equals("a") && (xcord - 1) >= 0){
+	    if (_map[ycord][xcord - 1].equals(" ")){
+		    return;}
+	    else setx(xcord - 1);
+	}
+	
+	if (dir.equals("d") && (xcord + 1) < 12){
+	    if (_map[ycord][xcord + 1].equals(" ")){
+		    return;}
+	    else setx(xcord + 1);
+	}
+    }
 	
 
     public static void main(String[] args){
     Map test = new Map();
-    System.out.println(test);
+    String holder = "";
+    while (holder != "EXIT"){
+	System.out.println(test);
+	try {
+	    holder = in.readLine();
+	}
+	catch ( IOException e ){ }
+	test.move(holder);
     }
-
+    }
 
 }//end map
     
