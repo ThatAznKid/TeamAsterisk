b0ee4abfc9c20b7d355dc67554cea18bad166056
ThatAznKid <jchen47@stuy.edu>
2016-01-20 09:39:38 -0500

    updated

--- Tile.java
+++ Tile.java
@@ -4,18 +4,18 @@ public class Tile{
     public Tile(){
 	type = "null";
     }    
-    public Tile(String in){
+    public Tile(Object in){
 	type = process(in);
     }
 
-    public String process(String key){
+    public String process(Object key){
 	if (key.equals("o") || key.equals("T") || key.equals("R") || key.equals("A") || key.equals("C")){
 	    return "town";
 	}
 	if (key.equals("+")){
 	    return "road";
 	}
-	if (key.equals("=")){
+	if (key.equals("=") || key.equals("|")){
 	    return "bridge";
 	}
 	if(key.equals("B")){
