57ee9fc617f152dd0f73578e8bf5347734919876
Richard <Richard>
2016-01-24 22:12:20 -0500

    test

---
+++ Monster1.java
@@ -0,0 +1,42 @@
+public class Monster extends Character {
+
+    public Monster() {
+	super();
+	_hitPts = 3;
+	_strength = 1;
+	_defense = 1;
+	Talisman = 0; 
+	Shield = 0; 
+	_level = 1;
+	_name = "Slime";
+    }
+
+    public String moveSet(){
+	return "";
+    }
+    
+    public void normalize(){
+    }
+
+    public void specialize(){
+    }
+
+    public void specialize2(){
+    }
+
+    public String about(){
+        return "";
+    }
+
+    public int attack ( Character opponent ) { 
+	int holder = opponent.getShield();
+	double modifier = 1 - ( holder / ( 50 + holder ));
+	int damage = (int) (modifier * _strength); 
+	if (damage < 0) { 
+	    damage = 1; 
+	} 
+	opponent.lowerHP (damage); 
+	return damage; 
+    }
+
+}//end class Monster
